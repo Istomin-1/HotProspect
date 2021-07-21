c30201c4225dd4a9692fe5abcb1409b89e447dea
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-07-21 21:46:10 +0500

    Initial Commit

---
+++ HotProspect/MeView.swift
@@ -0,0 +1,59 @@
+//
+//  MeView.swift
+//  HotProspect
+//
+//  Created by Mikhail on 21.07.2021.
+//
+
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct MeView: View {
+    @State private var name = "Istomin Mikhail"
+    @State private var emailAddress = "istomin-mihail-91@mail.ru"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+            }
+            .navigationBarTitle("Your code")
+        }
+    }
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+ }
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
---
+++ HotProspect/Prospect.swift
@@ -0,0 +1,49 @@
+//
+//  Prospect.swift
+//  HotProspect
+//
+//  Created by Mikhail on 21.07.2021.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    static let saveKey = "SavedData"
+    
+    @Published private(set) var people: [Prospect]
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+        
+        self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+}
